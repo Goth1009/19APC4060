d47a15bc599e435bff93febc96691fb0441c8da8
Gothnima Sathsarani <138848121+Goth1009@users.noreply.github.com>
2025-03-21 21:09:17 +0530

    Delete src/main directory

--- src/main/java/com/example/stringmanipulationservice/StringManipulationEndpoint.java
+++
@@ -1,9 +0,0 @@
-package com.example.stringmanipulationservice;
-
-import javax.xml.ws.Endpoint;
-
-public class StringManipulationEndpoint {
-    public static void main(String[] args) {
-        Endpoint.publish("http://localhost:8080/StringManipulationService", new StringManipulationServiceImpl());
-    }
-}
